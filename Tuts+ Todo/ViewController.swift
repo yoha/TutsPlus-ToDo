d96e69c284b2bc18960e7f53562180cc39dda2b0
Yohannes Wijaya <yohannes.wijaya@gmail.com>
2016-02-02 23:22:13 +0700

    "Swift from Scratch: Inheritance and Protocols": "Protocols"

--- Tuts+ Todo/ViewController.swift
+++ Tuts+ Todo/ViewController.swift
@@ -8,11 +8,43 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    // MARK: - Stored Properties
+    
+    var items = [String]()
+    
+    let tableViewCellIdentifier = "TableViewCellIdentifier"
 
     // MARK: - IBOutlet Properties
     
     @IBOutlet var tableView: UITableView!
+    
+    // MARK: - UITableViewDataSource Methods
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.items.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(self.tableViewCellIdentifier, forIndexPath: indexPath)
+        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegateMethods
+    
+    // MARK: - UIViewController Methods
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: self.tableViewCellIdentifier)
+        
+        self.items = ["Create Personal Project w/ Swift", "Teach people to program in Swift", "Share experience in Swift"]
+        
+        self.title = "ToDo"
+    }
 
 }
 
